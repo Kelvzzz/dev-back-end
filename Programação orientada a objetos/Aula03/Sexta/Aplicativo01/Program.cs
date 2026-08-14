5123abfb2f570bf796010824c979b35399c1e664
Kelvzzz <eukelvin.ati@gmail.com>
2026-08-14 19:26:03 -0300

    Versão 1

---
+++ Programação orientada a objetos/Aula03/Sexta/Aplicativo01/Program.cs
@@ -0,0 +1,20 @@
+﻿//Declaração de variaveis
+
+double raio, circunferencia, volume;
+const double pi = 3.1415;
+
+
+//Entrada de dados
+
+Console.Write("Entre com o valor do raio da circunferência: ");
+raio = double.Parse(Console.ReadLine());
+
+
+//Processamento de dados
+circunferencia = 2 * pi * raio;
+volume = (4 * pi * Math.Pow(raio, 3)) / 3;
+
+//Saida de dados
+Console.WriteLine($"Cicunferência: {circunferencia:f2}" +
+    $"Volume: {volume:f2}" +
+    $"PI = {pi:f2}");
