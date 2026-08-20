3c8aeb2fbbfab590f1957689fd359d4e45e20bb0
Kelvzzz <eukelvin.ati@gmail.com>
2026-08-19 21:55:32 -0300

    passado herança

---
+++ Programação orientada a objetos/Aula05/Quarta/Banco/Entidades/ContaPessoaFisica.cs
@@ -0,0 +1,57 @@
+﻿
+namespace Banco.Entidades
+{
+	internal class ContaPessoaFisica
+	{
+		private int numero;
+		private double saldo;
+
+
+		//Campos
+		protected int NumeroDaConta
+		{
+			get { return numero; }
+			set { numero = value; }
+		}
+
+		private string titular;
+
+		protected string TitularDaConta
+		{
+			get { return titular; }
+			set { titular = value; }
+		}
+		protected double SaldoDaConta
+		{
+			get { return saldo; }
+			set { saldo = value; }
+		}
+	
+	//Construtor 
+	public ContaPessoaFisica(string titularDaConta, int numeroDaConta)
+		{
+			TitularDaConta = titularDaConta;
+			NumeroDaConta = numeroDaConta;
+			SaldoDaConta = 0;
+		}
+
+        public ContaPessoaFisica(string titular, string titularDaConta, double saldoDaConta)
+        {
+            this.titular = titular;
+            TitularDaConta = titularDaConta;
+            SaldoDaConta = saldoDaConta;
+        }
+
+		//Metodos
+		public void Saque(double quantia)
+		{
+			SaldoDaConta -= quantia;
+		}
+		public void Deposito(double quantia)
+		{
+			SaldoDaConta += quantia;
+		}
+    }
+
+
+}
---
+++ Programação orientada a objetos/Aula05/Quarta/Banco/Entidades/ContaPessoaJuridica.cs
@@ -0,0 +1,33 @@
+﻿
+
+namespace Banco.Entidades
+{
+    internal class ContaPessoaJuridica : ContaPessoaFisica
+    {
+		//Campos
+		private double limite;
+
+        
+        //Construtor
+        public ContaPessoaJuridica(string titularDaConta, int numeroDaConta, double limite) : base(titularDaConta, numeroDaConta)
+        {
+            LimiteDaConta = limite;
+        }
+
+        public ContaPessoaJuridica(string titular, string titularDaConta, double saldoDaConta,double limite) : base(titular, titularDaConta, saldoDaConta)
+        {
+            LimiteDaConta = limite;
+
+        }
+        //Propriedades
+        protected double LimiteDaConta
+		{
+			get { return limite; }
+			set { limite = value; }
+		}
+        public void Limite(double quantia)
+        {
+            SaldoDaConta += quantia;
+        }
+	}
+}
